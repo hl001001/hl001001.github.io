af5b6ceddfd62f53711caa6a9ff2f9b0970d6560
xyy1733@163.com <xyy1733@163.com>
2025-05-06 17:24:45 +0800

    Site updated: 2025-05-06 17:24:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/18/hello-world/","2025/05/06/py格式转换软件/"];function toRandomPost(){
+var posts=["2025/05/06/py格式转换软件/","2025/04/18/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
